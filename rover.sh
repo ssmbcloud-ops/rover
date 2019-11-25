590bbc2e5de854b11b7449609a0c09cf4e471a88
BrettOJ <boj@audiant.com.au>
2019-11-25 10:15:10 +0800

    Update rover.sh

--- rover.sh
+++ rover.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -i --rm -v $(pwd)/.azure:/root/.azure -v ${HOME}/.terraform.d:/root/.terraform.d caf_rover $@
+docker run --rm -v $(pwd)/.azure:/root/.azure -v ${HOME}/.terraform.d:/root/.terraform.d caf_rover $@
